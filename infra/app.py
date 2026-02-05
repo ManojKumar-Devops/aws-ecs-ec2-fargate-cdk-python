28940e9c069ea00993e50fe62ea77199e3affe2d
ManojKumar <manojkumar26198@gmail.com>
2026-02-05 14:48:01 +0000

    feat: end-to-end EC2 ALB ASG hello world using AWS CDK (Python)

--- infra/app.py
+++ infra/app.py
@@ -3,14 +3,10 @@ import os
 import aws_cdk as cdk
 
 from stacks.network_stack import NetworkStack
-from stacks.ecr_stack import EcrStack
-from stacks.ecs_fargate_stack import EcsFargateStack
-from stacks.ecs_ec2_stack import EcsEc2Stack
-
+from stacks.ec2_alb_asg_hello_stack import Ec2AlbAsgHelloStack
 
 app = cdk.App()
 
-# Pick one of the allowed playground regions (us-east-1 / us-east-2 / us-west-2)
 env = cdk.Environment(
     account=os.getenv("CDK_DEFAULT_ACCOUNT"),
     region=os.getenv("CDK_DEFAULT_REGION"),
@@ -18,29 +14,6 @@ env = cdk.Environment(
 
 network = NetworkStack(app, "NetworkStack", env=env)
 
-ecr = EcrStack(app, "EcrStack", env=env)
-
-# Fargate path (simplest)
-fargate = EcsFargateStack(
-    app,
-    "EcsFargateStack",
-    vpc=network.vpc,
-    repository=ecr.repo,
-    env=env,
-)
-
-# EC2 path (capacity provider + mixed instances)
-ec2 = EcsEc2Stack(
-    app,
-    "EcsEc2Stack",
-    vpc=network.vpc,
-    repository=ecr.repo,
-    env=env,
-)
-
-# Optional: self-hosted GitHub runner on EC2
-# (Use only if your org allows runner registration and outbound to GitHub is permitted)
-# runner = GithubRunnerStack(app, "GithubRunnerStack", vpc=network.vpc, env=env)
+Ec2AlbAsgHelloStack(app, "Ec2AlbAsgHelloStack", vpc=network.vpc, env=env)
 
 app.synth()
-
---
+++ infra/stacks/ec2_alb_asg_hello_stack.py
@@ -0,0 +1,72 @@
+from constructs import Construct
+from aws_cdk import (
+    Stack,
+    CfnOutput,
+    Duration,
+    aws_ec2 as ec2,
+    aws_elasticloadbalancingv2 as elbv2,
+    aws_autoscaling as autoscaling,
+)
+
+class Ec2AlbAsgHelloStack(Stack):
+    def __init__(self, scope: Construct, construct_id: str, vpc: ec2.IVpc, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        # Security group for ALB
+        alb_sg = ec2.SecurityGroup(self, "AlbSg", vpc=vpc, allow_all_outbound=True)
+        alb_sg.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(80), "HTTP")
+
+        # ALB
+        alb = elbv2.ApplicationLoadBalancer(
+            self, "Alb",
+            vpc=vpc,
+            internet_facing=True,
+            security_group=alb_sg,
+            vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PUBLIC),
+        )
+
+        listener = alb.add_listener("HttpListener", port=80, open=True)
+
+        # Security group for instances (then allow only from ALB)
+        instance_sg = ec2.SecurityGroup(self, "InstanceSg", vpc=vpc, allow_all_outbound=True)
+        instance_sg.add_ingress_rule(alb_sg, ec2.Port.tcp(80), "HTTP from ALB only")
+
+        # UserData: install nginx and serve Hello World
+        user_data = ec2.UserData.for_linux()
+        user_data.add_commands(
+            "yum update -y",
+            "amazon-linux-extras install -y nginx1 || yum install -y nginx",
+            "echo 'Hello World from EC2 + ALB + ASG (CDK Python)!' > /usr/share/nginx/html/index.html",
+            "systemctl enable nginx",
+            "systemctl start nginx",
+        )
+
+        asg = autoscaling.AutoScalingGroup(
+            self, "Asg",
+            vpc=vpc,
+            vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PUBLIC),
+            instance_type=ec2.InstanceType("t3.micro"),
+            machine_image=ec2.MachineImage.latest_amazon_linux2(),
+            min_capacity=1,
+            desired_capacity=1,
+            max_capacity=2,
+            security_group=instance_sg,
+            user_data=user_data,
+        )
+
+        # Target group via listener
+        listener.add_targets(
+            "AppTargets",
+            port=80,
+            targets=[asg],
+            health_check=elbv2.HealthCheck(
+                path="/",
+                healthy_http_codes="200",
+                interval=Duration.seconds(30),
+            ),
+        )
+
+        # Basic scaling (CPU)
+        asg.scale_on_cpu_utilization("CpuScaling", target_utilization_percent=50)
+
+        CfnOutput(self, "AlbUrl", value=f"http://{alb.load_balancer_dns_name}")
