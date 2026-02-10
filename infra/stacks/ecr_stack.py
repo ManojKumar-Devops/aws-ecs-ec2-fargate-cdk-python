967e8a01f92f069defe221b057aa5cff532847ca
ManojKumar <manojkumar26198@gmail.com>
2026-02-10 10:17:46 +0000

    Added ECS and Fargate Deployment Model

--- infra/stacks/ecr_stack.py
+++ infra/stacks/ecr_stack.py
@@ -1,18 +1,4 @@
-from constructs import Construct
-from aws_cdk import (
-    Stack,
-    RemovalPolicy,
-    aws_ecr as ecr,
-)
+from aws_cdk import CfnOutput
 
-class EcrStack(Stack):
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
-
-        self.repo = ecr.Repository(
-            self, "HelloRepo",
-            repository_name="hello-world-lab",
-            removal_policy=RemovalPolicy.DESTROY,
-            empty_on_delete=True,
-            image_scan_on_push=True,
-        )
+CfnOutput(self, "RepoName", value=self.repo.repository_name)
+CfnOutput(self, "RepoUri", value=self.repo.repository_uri)
