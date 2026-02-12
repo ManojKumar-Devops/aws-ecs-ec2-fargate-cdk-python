59c4deeb8bf2077182dea64845182e097943be65
ManojKumar <manojkumar26198@gmail.com>
2026-02-12 09:57:01 +0000

    Enable ECS Blue/Green + self-hosted runners

--- infra/app.py
+++ infra/app.py
@@ -8,6 +8,7 @@ from stacks.ecr_stack import EcrStack
 from stacks.ec2_alb_asg_hello_stack import Ec2AlbAsgHelloStack
 from stacks.ecs_fargate_stack import EcsFargateStack
 # from stacks.ecs_ec2_stack import EcsEc2Stack
+from stacks.ecs_fargate_bluegreen_stack import EcsFargateBlueGreenStack
 
 app = cdk.App()
 
@@ -22,6 +23,7 @@ ecr = EcrStack(app, "EcrStack", env=env)
 # Context switches
 deploy_ec2_asg = app.node.try_get_context("deploy_ec2_asg") != "false"
 deploy_ecs = app.node.try_get_context("deploy_ecs") == "true"
+deploy_ecs_bg = app.node.try_get_context("deploy_ecs_bg") == "true"
 
 if deploy_ec2_asg:
     Ec2AlbAsgHelloStack(app, "Ec2AlbAsgHelloStack", vpc=network.vpc, env=env)
@@ -29,5 +31,8 @@ if deploy_ec2_asg:
 if deploy_ecs:
     EcsFargateStack(app, "EcsFargateStack", vpc=network.vpc, env=env)
     # EcsEc2Stack(app, "EcsEc2Stack", vpc=network.vpc, env=env)
+
+if deploy_ecs_bg:
+    EcsFargateBlueGreenStack(app, "EcsFargateBlueGreenStack", vpc=network.vpc, env=env)    
 
 app.synth()
